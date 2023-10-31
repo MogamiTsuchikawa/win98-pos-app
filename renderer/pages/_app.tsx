72a5c37eddd4ab399ca6719626127084d1cacfd9
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2023-10-31 22:05:41 +0900

    フォーマット

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,16 +1,16 @@
-import "../../node_modules/98.css/dist/98.css"
-import "../style/reset.css"
-import "../style/window.css"
-import WindowBar from "../components/Window/WindowBar"
+import "../../node_modules/98.css/dist/98.css";
+import "../style/reset.css";
+import "../style/window.css";
+import WindowBar from "../components/Window/WindowBar";
 
 const App = ({ Component, pageProps }) => {
   const windowStyle = {
-    height: "100%"
-  }
+    height: "100%",
+  };
   const bodyStyle = {
     width: "calc(100% - 5px)",
-    height: "calc(100vh - 45px)"
-  }
+    height: "calc(100vh - 45px)",
+  };
   return (
     <div className="window" style={windowStyle}>
       <WindowBar title="hello" />
@@ -18,8 +18,7 @@ const App = ({ Component, pageProps }) => {
         <Component {...pageProps} />
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
 export default App;
